e5bc020df095ab22a5c9f07ee04fb42880282a1c
Correy Everett <correy.everett@gmail.com>
2020-11-25 17:06:23 -0600

    working on the delete movie function

--- js/movies.js
+++ js/movies.js
@@ -2,23 +2,25 @@ $(document).ready(function (){
 
 
 // functions for 1. adding 2. editing 3. deleting
+    const url = 'https://tide-opalescent-sled.glitch.me/movies';
+    const moviesObject = fetch(`${url}`);
 
     function movieFetchRequest(){
-        var moviesObject=fetch('https://tide-opalescent-sled.glitch.me/movies');
-         moviesObject.then((response)=> response.json())
-                  .then( (data)=>{
-                      console.log(data);
-                  })
-             // .then( response => console.log(response) ) /* review was created successfully */
+         moviesObject
+             .then((response)=> response.json())
+             .then( (data)=>{
+                 console.log(data);
+             })
             .catch( error => console.error(error) ); /* handle errors */
     }
     console.log(movieFetchRequest());
+
 
 // function add a movie
 
 function addMovie() {
     const addNewMovie = {
-        actors: 'Chris Hemsworth, Robert Jnr.',
+        actors: 'Chris Hemsworth, Robert Downey Jr.',
         directors: "Joss Whedon",
         genre: "Fantasy",
         id: 5,
@@ -28,7 +30,7 @@ function addMovie() {
         title: 'Avengers',
         year: "2012"
     };
-    const url = 'https://tide-opalescent-sled.glitch.me/movies';
+    // const url = 'https://tide-opalescent-sled.glitch.me/movies';
     const options = {
         method: 'POST',
         headers: {
@@ -38,10 +40,49 @@ function addMovie() {
 
     };
 
-
 }
+
+    // console.log(addMovie());
+
+
+
+            const removeMovie = id => fetch(`${url}/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+
+                    .then((response)=> response.json())
+                    .then( (data)=>{
+                        console.log(data);
+                    })
+                    .catch( error => console.error(error) ) /* handle errors */
+
+        
+
+
+
+
+
+
+
+
+
 
 
 
 //    ending document.ready
 });
+
+// const deleteDog = id => fetch(`${apiURL}/${id}`, {
+//     method: 'DELETE',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     }
+// })
+//     .then(res => res.json())
+//     .then(() => {
+//         console.log(`Success: deleted dog with id of ${id}`);
+//     })
+//     .catch(console.error);
