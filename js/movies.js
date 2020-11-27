855bc7c2543c2da065cf2265dc017bc32eb888c8
jaya <jayaseyyadri@gmail.com>
2020-11-27 17:22:14 -0600

    pending features edit movie and add movie dynamically

--- js/movies.js
+++ js/movies.js
@@ -1,38 +1,46 @@
-$(document).ready(function (){
 let j;
-    const url = 'https://tide-opalescent-sled.glitch.me/movies'
+let DEBUG = false;
+const url = 'https://tide-opalescent-sled.glitch.me/movies'
 
-    //  <----functions for 1. adding 2. editing 3. deleting----->
+//  <----functions for 1. adding 2. editing 3. deleting----->
 
-    const moviesObject = fetch(`${url}`)
 
+
+
+//----------- loading------------------------
+function movieFetchRequest(){
+    const moviesObject = fetch(url);
     let loadingmsg = $('<div>');
     let loadEl = $('#load');
     loadingmsg.html("<b>loading</b>");
     loadEl.append(loadingmsg);
 
-    //----------- loading------------------------
-    function movieFetchRequest(){
-         moviesObject
-             .then(response =>response.json())
-             .then( data => {
-                  let startHtml = ''
-                     for(let i=0 ;i<5 ;i++){
-                        startHtml += renderMovieCards(data,i);
-                 }
-                 $('#main-container').html(startHtml)
-             })
-             .finally(f => {
-                     loadingmsg.remove();
-                 });
+    moviesObject
+        .then(response =>response.json())
+        .then( data => {
+            renderCards(data);
+            if(DEBUG) {
+                console.log(data);
+            }
+        })
+        .finally(_ => {
+            loadingmsg.remove();
+        });
+    //Promise completed
+}
+
+function renderCards(data) {
+    let startHtml = ''
+    for(let i=0 ;i<data.length ;i++){
+        startHtml += renderMovieCards(data[i]);
     }
-
-    console.log(movieFetchRequest());
-
+    $('#main-container').html(startHtml);
+    return data;
+}
 // <-----------function add a movie------------------->
 
 function addMovie() {
-    const addNewMovie = {
+    const newMovieObj = {
         title: "tenet",
         rating: "5",
         poster: "https://m.media-amazon.com/images/M/MV5BYzg0NGM2NjAtNmIxOC00MDJmLTg5ZmYtYzM0MTE4NWE2NzlhXkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_SX300.jpg",
@@ -41,63 +49,66 @@ function addMovie() {
         director: "Christopher Nolan",
         plot: "Armed with only one word, Tenet, and fighting for the survival of the entire world, a Protagonist journeys through a twilight world of international espionage on a mission that will unfold in something beyond real time.",
         actors: "Elizabeth Debicki, Robert Pattinson, John David Washington, Aaron Taylor-Johnson",
-        id: 3
     };
     const options = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(addNewMovie),
-
+        body: JSON.stringify(newMovieObj),
     };
+    // insert new movie
+    fetch(url,options).then(movieFetchRequest);
+};
 
+
+//<-----------------------function to delete a movie----------------------->
+function deleteMovie(id) {
+    const removeMovie  = fetch(`${url}/${id}`, {
+
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then((response) => response.json()).then(movieFetchRequest)
+        .catch(error => console.error(error)) /* handle errors */
 }
-
-    console.log(addMovie());
-
-    //<-----------------------function to delete a movie----------------------->
-            function deleteMovie() {
-
-                const removeMovie  = fetch(`${url}/id`, {
-
-                    method: 'DELETE',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    }
-                })
-                    .then((response) => response.json())
-                    .catch(error => console.error(error)) /* handle errors */
-            }
 //function to edit a movie
 
 
-    function editMovie(){
-        const editMovieInfo = fetch(`${url}/id`, {
+function editMovie(){
+    const editMovieInfo = fetch(`${url}/id`, {
 
         method:'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        })
-            .then((response) => response.json())
-            .catch(error => console.error(error)) /* handle errors */
-    }
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        //body: JSON.stringify(newMovieObj)
+    })
+        .then((response) => response.json())
+        .catch(error => console.error(error)) /* handle errors */
+}
 
 
-//____________________Render cards------------------
-    function  renderMovieCards(data,j) {
-           let html= `<div class="card">`
-               html+=`
-                 <img class="card-img-top" src="" alt="Card image top">
+//<-----------------------------Render cards-------------------------->
+function  renderMovieCards(movie) {
+    let html= `<div class="card" >`
+    html+=`
+                 <img class="card-img-top" style="width: 200px" src=${movie.poster} alt="Card image top">
                  <div class="card-body">
-                        <h3 class="card-title">Movie title</h3>
+                        <h3 class="card-title">${movie.title}</h3>
                         <h4 class="card-subtitle">Movie Description</h4>
                         <p class="card-text">This is a simple Card example</p>
+                        <p><button onclick='deleteMovie(${movie.id})'>Delete</button></p>
                      </div></div>`
-                   return html;
-    }
-//    ending document.ready
+    return html;
+}
+
+$(document).ready(function (){
+
+    movieFetchRequest();
+    $('#addMovieBtn').on('click', addMovie);
 
 });
 
